dbe585e9a264beaa71b9ce7d466d63fe50fdb303
iuliia <juliyakuzya@gmail.com>
2023-09-05 16:48:02 +0300

    src/

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         int initialBalanse = 500;
-        int addend = 1700;
+        int addend = 700;
 
         int bonus = 0;
-        if (addend > 1000) ;
-        {
+        if (addend > 1000) {
             bonus = addend / 100;
         }
         int finalBalans = initialBalanse + addend + bonus;
         System.out.println("Итоговый баланс: " + finalBalans);
-        System.out.println("Бонус: + bonus");
+        System.out.println("Бонус:" + bonus);
 
     }
 }
